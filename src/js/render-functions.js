b1317b090677985df902aea78f85e25bd77e7538
Anton Shuvalov <anshuvalov83@gmail.com>
2025-09-12 18:23:12 +0300

    finish task

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,21 +1,68 @@
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-import 'https://cdnjs.cloudflare.com/ajax/libs/css-loader/2.3.0/css-loader.min.css';
+// import 'https://cdnjs.cloudflare.com/ajax/libs/css-loader/2.3.0/css-loader.min.css';
 
-const lightbox = new SimpleLightbox('.gallery a', {});
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 
 const listEl = document.querySelector('.gallery');
 const loaderEl = document.querySelector('.loader');
 
+export function createGallery(arr) {
+  return arr
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        return `<li class="gallery-item">
+        <a class="gallery-link" href="${largeImageURL}">
+          <img
+            class="gallery-image"
+            src="${webformatURL}"
+            alt="${tags}"
+          />
+          <div class="info">
+            <div class="info-item">
+              <span class="info-label">Likes</span>
+              <span class="info-value">${likes}</span>
+            </div>
+            <div class="info-item">
+              <span class="info-label">Views</span>
+              <span class="info-value">${views}</span>
+            </div>
+            <div class="info-item">
+              <span class="info-label">Comments</span>
+              <span class="info-value">${comments}</span>
+            </div>
+            <div class="info-item">
+              <span class="info-label">Downloads</span>
+              <span class="info-value">${downloads}</span>
+            </div>
+          </div>
+        </a>
+      </li>`;
+      }
+    )
+    .join('');
+}
+
 export function clearGallery() {
-  listEl.textContent = '';
+  listEl.innerHTML = '';
 }
 
 export function showLoader() {
-  loader.classList.remove('is-hiden');
+  loaderEl.classList.remove('is-hidden');
 }
 
 export function hideLoader() {
-  loader.classList.add('is-hiden');
+  loaderEl.classList.add('is-hidden');
 }
--- src/main.js
+++ src/main.js
@@ -2,6 +2,46 @@ import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
 import { getImagesByQuery } from './js/pixabay-api.js';
+import {
+  createGallery,
+  clearGallery,
+  showLoader,
+  hideLoader,
+} from './js/render-functions.js';
+
+const listEl = document.querySelector('.gallery');
+const formEl = document.querySelector('.form');
+
+formEl.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  clearGallery();
+  showLoader();
+
+  const inputQuerry = event.currentTarget.elements['search-text'].value.trim();
+
+  getImagesByQuery(inputQuerry)
+    .then(response => {
+      const photosArr = response;
+
+      if (photosArr.length === 0) {
+        iziToast.error({
+          title: 'Error',
+          message: `❌ Sorry, there are no images matching your search query. Please try again!`,
+        });
+        return;
+      }
+
+      listEl.innerHTML = createGallery(photosArr);
+      gallery.refresh();
+    })
+    .catch(error => {})
+    .finally(() => {
+      hideLoader();
+      formEl.reset();
+    });
+}
 
 getImagesByQuery('car')
   .then(response => {
